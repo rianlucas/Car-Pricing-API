b6f6732003c5b79733b94319463453dfdb3f74eb
rian lucas <rianluccas08@gmail.com>
2023-06-06 18:51:39 -0300

    feat: use repository and create and save a User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-users.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  creteUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
+}
