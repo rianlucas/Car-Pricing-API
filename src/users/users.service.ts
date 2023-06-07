1ccaa249989815fc103ba272d004a68656ee1fb1
rian lucas <rianluccas08@gmail.com>
2023-06-07 18:16:19 -0300

    feat: finished the users Service Crud

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -11,5 +11,32 @@ export class UsersService {
     const user = this.repository.create({ email, password });
 
     return this.repository.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repository.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repository.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.repository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.repository.remove(user);
   }
 }
