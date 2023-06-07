1b22d3eeb859d7af98b5472fd20666e8582c858a
rian lucas <rianluccas08@gmail.com>
2023-06-07 18:17:23 -0300

    feat: add logs to User entity and create a update User dto

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { AfterInsert, AfterRemove, AfterUpdate } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +11,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
 }
